be984b9c0197671a3ee654b840059d11173d78ea
Кирилл Миронов <k.mironov03@gmail.com>
2025-01-18 18:22:02 +0300

    set up cmake, clang-format & gitignore

---
+++ include/custom_string.h
@@ -0,0 +1,7 @@
+#include <cstddef>  //std::size_t
+
+namespace kirillidk_containers {
+
+class String {};
+
+}  // namespace kirillidk_containers
---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include "custom_string.h"
+
+int main() {
+    kirillidk_containers::String str;
+    return 0;
+}
