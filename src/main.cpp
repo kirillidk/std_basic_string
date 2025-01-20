5041478e7469a12283cb820c76ad68fe528481a2
Кирилл Миронов <k.mironov03@gmail.com>
2025-01-20 10:48:21 +0300

    added basic String methods, implementation of them & some docs

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,17 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
 #include "custom_string.h"
 
 int main() {
-    kirillidk_containers::String str;
+    kirillidk_containers::string str;
+
+    std::string s;
+
+    std::cout << str.capacity() << std::endl;
+
+    std::cout << str[0] << " " << str[1] << " " << str[15];
+
     return 0;
 }
